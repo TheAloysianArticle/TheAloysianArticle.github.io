9c9284b1cccaf940961faec23a567cb3d21c6d64
BookSalmon <123578256+HaxeFloppa@users.noreply.github.com>
2025-03-02 12:43:20 +0000

    something to do about exports??

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import supabase from 'https://unpkg.com/@supabase/supabase-js@2'
+import { object } from 'https://unpkg.com/@supabase/supabase-js@2'
 const supabased = createClient('https://rivaadrywxieeuqkyjjk.supabase.co', process.env.SUPABASE_KEY);
 
 async function fetchKeys() {
