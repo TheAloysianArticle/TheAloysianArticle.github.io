8b14774ee4c52083bf92e62560e843fcef7ab931
BookSalmon <123578256+HaxeFloppa@users.noreply.github.com>
2025-03-02 12:14:03 +0000

    i genuinely hate javascript so much

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import { createClient } from '../node_modules/@supabase/supabase-js/src/index.ts'
+// import { createClient } from '../node_modules/@supabase/supabase-js/src/index.ts'
 
 const supabase = createClient('https://rivaadrywxieeuqkyjjk.supabase.co', process.env.SUPABASE_KEY);
 
 async function fetchKeys() {
     const { data, error } = await supabase.from('Keys').select();
-    console.log(data);
-    return 0;
+    if (error) {
+        console.log(error)
+    }
+    if (data) {
+        console.log(data)
+    }
 };
