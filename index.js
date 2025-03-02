a9dc6cebbd64475c56d7c3541aceba13a6e216ae
BookSalmon <123578256+HaxeFloppa@users.noreply.github.com>
2025-03-02 12:23:07 +0000

    fixing a syntax error

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2'
+import supabase from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2'
 // import { createClient } from '../node_modules/@supabase/supabase-js/src/index.ts'
 
-const supabase = createClient('https://rivaadrywxieeuqkyjjk.supabase.co', process.env.SUPABASE_KEY);
+const supabased = createClient('https://rivaadrywxieeuqkyjjk.supabase.co', process.env.SUPABASE_KEY);
 
 async function fetchKeys() {
-    const { data, error } = await supabase.from('Keys').select();
+    const { data, error } = await supabased.from('Keys').select();
     if (error) {
         console.log(error)
     }
